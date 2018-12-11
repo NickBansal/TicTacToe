a40143a78ccd430691fae96a72e7dbcf90a12466
Nick Bansal <https://github.com/NickBansal>
2018-12-11 11:27:13 +0000

    Enzyme testing set up

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../TicTacToe';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+configure({ adapter: new Adapter() });
+
+describe('<App />', () => {
+  let wrapper
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  })
+
+  it('includes 1 div with the className of App', () => {
+    expect(wrapper.find('div.App').exists()).toBe(true)
+  })
+  it('includes 2 divs', () => {
+    expect(wrapper.find('div.App h1').text()).toBe("Tic Tac Toe")
+  })
+  it('Component matches the snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+
+})
